d8c577e2fb0d324bd0ef54bf645060696e064edc
NALH-nalh <tsujii.naruhiko.tn@gmail.com>
2025-06-19 13:14:50 +0900

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -18,11 +18,85 @@ sliderimage.alt = sliderimages[currentindex];
 prev.addEventListener("click",()=>{
     currentindex = (currentindex-1+sliderimages.length)%sliderimages.length;
     sliderimage.src = sliderimages[currentindex];
-    sliderimage.alt = sliderimages[currentindex];  
+    sliderimage.alt = sliderimages[currentindex];
+
+    sliderimage.classList.remove("fade-anim");
+    void sliderimage.offsetWidth;
+    sliderimage.classList.add("fade-anim");  
 });
 
 next.addEventListener("click",()=>{
     currentindex = (currentindex+1)%sliderimages.length;
     sliderimage.src = sliderimages[currentindex];
     sliderimage.alt = sliderimages[currentindex];  
+
+        sliderimage.classList.remove("fade-anim");
+    void sliderimage.offsetWidth;
+    sliderimage.classList.add("fade-anim"); 
 });
+
+const intervalTime = 3000;
+
+setInterval(() => {
+    currentindex = (currentindex + 1) % sliderimages.length;
+    sliderimage.src = sliderimages[currentindex];
+    sliderimage.alt = sliderimages[currentindex];
+
+    sliderimage.classList.remove("fade-anim");
+    void sliderimage.offsetWidth;
+    sliderimage.classList.add("fade-anim"); 
+}, intervalTime);
+
+
+const profileimage = document.querySelectorAll("#profile-image");
+const Programmingimage = document.querySelectorAll("#Programming-image");
+const Writingimage = document.querySelectorAll("#Writing-image");
+const videoedtingimage = document.querySelectorAll("#video-edting-image");
+const illustraitonimage = document.querySelectorAll("#illustraiton-image");
+const imageeditingimage = document.querySelectorAll("#image-editing-image");
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("visible");
+        }else{
+            entry.target.classList.remove("visible");
+        };
+    });
+},{threshold:0.3});
+
+profileimage.forEach(el => observer.observe(el));
+Programmingimage.forEach(el => observer.observe(el));
+Writingimage.forEach(el => observer.observe(el));
+videoedtingimage.forEach(el => observer.observe(el));
+illustraitonimage.forEach(el => observer.observe(el));
+imageeditingimage.forEach(el => observer.observe(el));
+
+
+
+
+
+const profileskill = document.querySelectorAll("#profile-skill");
+const Programmingskill = document.querySelectorAll("#Programming-skill");
+const Writingskill = document.querySelectorAll("#Writing-skill");
+const videoedtingskill = document.querySelectorAll("#video-edting-skill");
+const illustraitonskill = document.querySelectorAll("#illustraiton-skill");
+const imageeditingskill = document.querySelectorAll("#image-editing-skill");
+
+const observer2 = new IntersectionObserver((entries) =>{
+entries.forEach(entry=>{
+    if(entry.isIntersecting){
+        entry.target.classList.add("invisible");
+    }else{
+        entry.target.classList.remove("invisible");
+    };
+});
+},{threshold:0.3});
+
+
+profileskill.forEach(le => observer2.observe(le));
+Programmingskill.forEach(le => observer2.observe(le));
+Writingskill.forEach(le => observer2.observe(le));
+videoedtingskill.forEach(le => observer2.observe(le));
+illustraitonskill.forEach(le => observer2.observe(le));
+imageeditingskill.forEach(le => observer2.observe(le));
